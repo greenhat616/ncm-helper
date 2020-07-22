26a727099f309cff71cd4cf1eef81e03d6ee13e3
a632079 <a632079@qq.com>
2020-07-22 17:39:59 +0800

    refactor: config

--- src/config/default.go
+++ src/config/default.go
@@ -6,6 +6,7 @@ func setServerDefault() {
 	viper.SetDefault("server.port", 8000)
 }
 
+// SetDefault is a func will be called when init, injecting the defeat values of config
 func SetDefault() {
 	setServerDefault()
 }
